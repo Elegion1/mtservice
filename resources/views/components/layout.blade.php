07005a183ce8a91bc877100888b26614372a72da
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-08-06 17:47:01 +0200

    ottimizzazione seo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
+        @php
+            $title = '';
+            $type = '';
+            $currentRouteName = Route::currentRouteName();
+        @endphp
         @foreach ($pages as $page)
-            @if (Route::currentRouteName() == $page->link)
-                {{ ucfirst(__('ui.' . $page->name)) }}
+            @if ($currentRouteName == $page->link)
+                @php
+                    $title = ucfirst(__('ui.' . $page->name));
+                    break; // Exit the loop if match is found
+                @endphp
             @endif
-        @endforeach | TRANCHIDA Transfer & Rent
+        @endforeach
+        @if (empty($title))
+            @php
+                // Ottenere il valore di PATH_INFO
+                $pathInfo = request()->getPathInfo();
+                // Suddividere il percorso in parti
+                $segments = explode('/', $pathInfo);
+                // Estrarre la parte desiderata (in questo caso, la seconda parte)
+                if (isset($segments[2])) {
+                    $title = urldecode($segments[2]);
+                    $type = urldecode($segments[1]);
+                }
+            @endphp
+        @endif
+        {{ $title }} | {{ ucfirst($type) }} | TRANCHIDA Transfer & Rent
     </title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -20,7 +42,6 @@
     <x-whatsapp />
     <x-nav />
     <x-masthead />
-
     <div class="position-absolute masthead-position">
         <div class="container-fluid">
             <x-display-error />
