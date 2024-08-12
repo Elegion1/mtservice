6069e120b7efbbf4e8e34e6358af6eb2185ead26
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-08-12 12:22:41 +0200

    aggiunta consenso cookies

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,19 +5,21 @@
         <x-display-message />
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control form_input_focused" id="email"
-                    aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control form_input_focused" id="password">
+            <div class="row">
+                <div class="mb-3 col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control form_input_focused" id="email"
+                        aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3 col-12">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control form_input_focused" id="password">
+                </div>
+                <div class="d-flex justify-content-center align-items-center col-12">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
             </div>
 
-            <div class="d-flex justify-content-between">
-                <button type="submit" class="btn btn-primary">Login</button>
-            </div>
         </form>
     </div>
 </x-dashboard-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="ed7dba31-d3d3-4dbf-a434-4fc9d4b7c659" data-blockingmode="auto" type="text/javascript"></script>
+    @include('cookie-consent::index')
     <title>
         @php
             $title = '';
