25b9c0fb63ec904f80c2ab9015ae40b228e62245
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-08-13 12:22:12 +0200

    aggiunto h1 nell'header

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -8,7 +8,8 @@
         @foreach ($contents as $content)
             @if ($content->page->link == $currentRoute)
                 @if ($content->images->isNotEmpty() && $content->order == 0)
-                    <img class="img_car" src="{{ Storage::url($content->images->first()->path) }}" alt="HEADER-IMG-{{$currentRoute}}">
+                    <img class="img_car" src="{{ Storage::url($content->images->first()->path) }}"
+                        alt="HEADER-IMG-{{ $currentRoute }}">
                     @php
                         $imageDisplayed = true;
                         break;
@@ -18,7 +19,15 @@
         @endforeach
 
         @if (!$imageDisplayed)
-            <img class="img_car" src="https://picsum.photos/1920/1080" alt="Default Image">
+            <img class="img_car" src="https://tranchidatransfer.it/storage/images/rDbh790BjCjSxKQKIGWeV5COjit8n53cGkOqYbat.jpg" alt="Default Image">
         @endif
+        
+        <div class="position-absolute text_masthead translate-middle text-white text-center">
+            @if ($content->page->link == $currentRoute && $content->order == 0)
+                <h1 class="text-d text-shadow text-responsive mt-5">
+                    {!! strtoupper($content->{'title_' . app()->getLocale()}) !!}</h1>
+                <p class="text-shadow btn_font_size">{{ $content->{'subtitle_' . app()->getLocale()} }}</p>
+            @endif
+        </div>
     </div>
 </header>
