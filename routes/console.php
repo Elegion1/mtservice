ffd8c647bf74d35b44a49f699c27722753ac467c
Giovanni Sugamiele <gionnymiele@MacBook-Pro-Elegion.local>
2024-08-30 21:20:29 +0200

    sitemap

--- routes/console.php
+++ routes/console.php
@@ -13,11 +13,22 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('sitemap:generate', function () {
-    
+
+    // Percorso della sitemap
+    $sitemapPath = public_path('sitemap.xml');
+
+    // Verifica se il file della sitemap esiste e lo elimina
+    if (file_exists($sitemapPath)) {
+        unlink($sitemapPath);
+        $this->comment('Vecchia sitemap cancellata.');
+    }
+
+    // Genera la nuova sitemap
     $sitemap = SitemapGenerator::create('https://www.tranchidatransfer.it');
 
-    $sitemap->writeToFile(public_path('sitemap.xml'));
-    
+    // Scrive la nuova sitemap nel file
+    $sitemap->writeToFile($sitemapPath);
+
     $this->comment('Sitemap generata con successo.');
 });
 
